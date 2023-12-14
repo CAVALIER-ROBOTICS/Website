12ab327ec07974b442a5ebeabdef815d2be76737
3x71us1v3 <70909117+3x71us1v3@users.noreply.github.com>
2023-12-13 19:50:26 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//Test 2
+//In my head in my heaaheahheaheaaad zombie zombue zombie eh eh 
 
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.nav-menu');
