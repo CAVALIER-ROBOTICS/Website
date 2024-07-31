da13a8e23e1d4e35ebb709219a8d6328fa8f9578
3x71us1v3 <you@example.com>
2024-07-31 13:26:21 -0700

    fixed js error

--- app.js
+++ app.js
@@ -3,8 +3,13 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.nav-menu');
 
-menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
-    console.log("Test");
-})
+if(menu != null) {
+    menu.addEventListener('click', function() {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.toggle('active');
+        console.log("Test");
+    })  
+} else {
+    console.log("Menu null")
+}
+
